817079bb2629bb8d73ff0011991e5fc7e911bf60
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-25 13:01:55 +0300

    95. Switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from './components/navbar';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/home';
+import Products from './components/products';
+import Posts from './components/posts';
+import Dashboard from './components/admin/dashboard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <hr />
+        <div className="fw-light">
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
